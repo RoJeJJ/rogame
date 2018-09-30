75291fc56c655a93e54a452bce192b76556b05ab
RoJe <fiekajie@163.com>
2018-09-30 19:07:52 +0800

    init commit

--- Master-Server/src/main/java/com/rojee/master/configuration/WebSecurityConfigurer.java
+++ Master-Server/src/main/java/com/rojee/master/configuration/WebSecurityConfigurer.java
@@ -1,13 +1,11 @@
-package com.rojee.configuration;
+package com.rojee.master.configuration;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
 @EnableWebSecurity
-public class ServerSecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
